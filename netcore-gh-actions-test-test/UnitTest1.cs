da649c20a871174c2f4273e56ae30a9afeba116b
JP <jpmercado@bookwana.com>
2021-01-15 09:20:46 +0800

    init commit with broken test and outdated packages

---
+++ netcore-gh-actions-test-test/UnitTest1.cs
@@ -0,0 +1,19 @@
+using Xunit;
+
+namespace netcore_gh_actions_test_test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(false);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            Assert.True(true);
+        }
+    }
+}
