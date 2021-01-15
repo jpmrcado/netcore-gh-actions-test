4173efa5e3120b9a69934d74e14c9b8f0209bec7
JP <jpmercado@bookwana.com>
2021-01-15 10:25:32 +0800

    add more test projects

---
+++ netcore-gh-actions-test-test-2/UnitTest2.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace netcore_gh_actions_test_test_2
+{
+    public class UnitTest2
+    {
+        [Fact]
+        public void Test2()
+        {
+            Assert.False(false);
+        }
+    }
+}
